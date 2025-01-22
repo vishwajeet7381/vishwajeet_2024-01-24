c8d45e75e2027aa472f20a4d1dbd24d322c9be2f
Vishwajeet Prasad <84278680+vishwajeet7381@users.noreply.github.com>
2025-01-22 19:29:12 +0530

    feat: add SetupDatabase class for table creation and data loading from CSV

---
+++ app/utils/setup_database.py
@@ -0,0 +1,72 @@
+import csv
+from pathlib import Path
+
+from app.database import DatabaseManager
+
+
+class SetupDatabase:
+    current_file_path: Path = Path(__file__)
+
+    @classmethod
+    def create_tables(cls):
+        sql_file_path = cls.current_file_path.parents[1] / Path("sql/tables.sql")
+
+        with sql_file_path.open("rt") as file_handler:
+            queries = file_handler.read()
+
+            with DatabaseManager() as db_cur:
+                db_cur.execute(queries)
+
+    @classmethod
+    def load_data(
+        cls, dataset_dir_path: str | Path | None = None, *file_names: str | Path
+    ):
+        dataset_dir_path_ = (
+            Path(dataset_dir_path)
+            if dataset_dir_path
+            else cls.current_file_path.parents[2] / "dataset"
+        )
+
+        store_status_file_path = dataset_dir_path_ / (
+            Path(file_names[0]) if len(file_names) > 0 else "store_status.csv"
+        )
+        business_hours_file_path = dataset_dir_path_ / (
+            Path(file_names[1]) if len(file_names) > 1 else "store_business_hours.csv"
+        )
+        store_timezone_file_path = dataset_dir_path_ / (
+            Path(file_names[2]) if len(file_names) > 2 else "store_timezone.csv"
+        )
+
+        with DatabaseManager() as db_cur:
+            with store_status_file_path.open("rt", newline="") as file_handler:
+                csv_reader = csv.reader(file_handler)
+
+                next(csv_reader)
+
+                with db_cur.copy(
+                    "COPY store_status (store_id, status, timestamp_utc) FROM STDIN"
+                ) as copy:
+                    for record in csv_reader:
+                        copy.write_row(record)
+
+            with business_hours_file_path.open("rt", newline="") as file_handler:
+                csv_reader = csv.reader(file_handler)
+
+                next(csv_reader)
+
+                with db_cur.copy(
+                    "COPY business_hours (store_id, day_of_week, start_time_local, end_time_local) FROM STDIN"
+                ) as copy:
+                    for record in csv_reader:
+                        copy.write_row(record)
+
+            with store_timezone_file_path.open("rt", newline="") as file_handler:
+                csv_reader = csv.reader(file_handler)
+
+                next(csv_reader)
+
+                with db_cur.copy(
+                    "COPY store_timezone (store_id, timezone_str) FROM STDIN"
+                ) as copy:
+                    for record in csv_reader:
+                        copy.write_row(record)
