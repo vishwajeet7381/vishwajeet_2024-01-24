90592ee475a5f0e79bcffc405f734a0c6f029813
Vishwajeet Prasad <84278680+vishwajeet7381@users.noreply.github.com>
2025-01-24 12:02:59 +0530

    feat: update Config class to use lowercase attributes and add flags for table creation and data loading

--- app/config.py
+++ app/config.py
@@ -2,8 +2,11 @@ import os
 
 
 class Config:
-    POSTGRESQL_HOST = os.environ.get("SM_POSTGRESQL_HOST")
-    POSTGRESQL_PORT = os.environ.get("SM_POSTGRESQL_PORT")
-    POSTGRESQL_DBNAME = os.environ.get("SM_POSTGRESQL_DBNAME") or "postgres"
-    POSTGRESQL_USER = os.environ.get("SM_POSTGRESQL_USER") or "postgres"
-    POSTGRESQL_PASSWORD = os.environ.get("SM_POSTGRESQL_PASSWORD")
+    postgresql_host = os.environ.get("SM_POSTGRESQL_HOST")
+    postgresql_port = os.environ.get("SM_POSTGRESQL_PORT")
+    postgresql_dbname = os.environ.get("SM_POSTGRESQL_DBNAME") or "postgres"
+    postgresql_user = os.environ.get("SM_POSTGRESQL_USER") or "postgres"
+    postgresql_password = os.environ.get("SM_POSTGRESQL_PASSWORD")
+
+    create_tables = False
+    load_data = False
